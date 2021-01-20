e244736f509d1828de9750b65d7907554f8747fa
giokey-gv <66443911+giokey-gv@users.noreply.github.com>
2021-01-19 19:17:35 -0500

    Creacion de  Servicio API

---
+++ src/app/services/pokemon.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PokemonService {
+
+  baseUrl: string = environment.baseUrl;
+
+  constructor(private http: HttpClient) { }
+
+  //Con esto se Obtiene el Pokemon
+  getPokemons(index){
+    return this.http.get<any>('${this.baseUrl}/pokemon/${index}');
+  }
+  
+}
