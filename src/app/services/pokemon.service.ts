1c88cf863dac440e803c57ddfa500f4835545a8c
luvasquez5 <luvasquez5@poligran.edu.co>
2021-01-27 17:55:30 -0500

    cambios minimos

--- src/app/services/pokemon.service.ts
+++ src/app/services/pokemon.service.ts
@@ -7,7 +7,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class PokemonService {
 
-  baseUrl: string = environment.baseUrl;
+  baseUrl = environment.baseUrl;
 
   constructor(private http: HttpClient) { }
 
