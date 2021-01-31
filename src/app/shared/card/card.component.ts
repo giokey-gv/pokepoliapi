06f9ec07be5fbf6042b3824c9266c9ef039ab0b0
luvasquez5 <luvasquez5@poligran.edu.co>
2021-01-31 15:38:00 -0500

    Imagen y estadsiticas del pokemon en card

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -11,7 +11,15 @@ export class CardComponent implements OnInit {
   pokemon:any = '';
   pokemontype = [];
   pokemonimg = '';
-
+  pokemonart = '';
+  pokemonfront = '';
+  pokemonback = '';
+  pokemonstats1 = [];
+  pokemonstats2 = [];
+  pokemonstats3 = [];
+  pokemonstats4 = [];
+  pokemonstats5 = [];
+  pokemonstats6 = [];
   constructor(private PokemonService: PokemonService, private ActivatedRoute: ActivatedRoute){
     this.ActivatedRoute.params.subscribe(
       params => {
@@ -27,7 +35,15 @@ export class CardComponent implements OnInit {
       res =>{
         this.pokemon = res
         this.pokemonimg = this.pokemon.sprites.front_default;
+        this.pokemonart = this.pokemon.sprites.other.dream_world.front_default;
+        this.pokemonback = this.pokemon.sprites.back_default;
         this.pokemontype = res.types[0].type.name;
+        this.pokemonstats1 = res.stats[0].base_stat;
+        this.pokemonstats2 = res.stats[1].base_stat;
+        this.pokemonstats3 = res.stats[2].base_stat;
+        this.pokemonstats4 = res.stats[3].base_stat;
+        this.pokemonstats5 = res.stats[4].base_stat;
+        this.pokemonstats6 = res.stats[5].base_stat;
         console.log(res);
       }
     );
