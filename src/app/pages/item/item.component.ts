ab43055a0525257b2c0e87696cd3c42cd395137c
giokey-gv <66443911+giokey-gv@users.noreply.github.com>
2021-02-04 22:33:46 -0500

    Estilo About

--- src/app/pages/item/item.component.ts
+++ src/app/pages/item/item.component.ts
@@ -1,4 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+
+import { PokemonService } from 'src/app/services/pokemon.service';
+import { MatTableDataSource } from '@angular/material/table';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-item',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ItemComponent implements OnInit {
 
-  constructor() { }
+  constructor(private pokemonService: PokemonService, private router: Router) { }
 
-  ngOnInit(): void {
+    ngOnInit(): void {
+    this.getPokemons();
   }
+// TRAE Y MUESTRAPOKEMON
+  getPokemons() {
+    let pokemonData;
 
+    for (let i = 1; i <= 300; i++) {
+      this.pokemonService.getPokemons(i).subscribe(
+        res => {
+          pokemonData = {
+            position: i,
+            image: res.sprites.front_default,
+            name: res.name
+          };
+
+          this.data.push(pokemonData);
+          this.dataSource = new MatTableDataSource<any>(this.data);
+        },
+        err => {
+          console.log(err);
+        }
+      );
+    }
+  }
 }
